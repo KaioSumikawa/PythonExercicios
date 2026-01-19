bd649f846d7a0956b8d1323e060015873bc197a0
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-01-20 05:22:45 +0900

    Exercício 11

---
+++ ex011.py
@@ -0,0 +1,7 @@
+larg = float(input('Qual a largura da parede? '))
+alt = float(input('Qual a altura da parede? '))
+área = larg * alt
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m2.'.format(larg, alt, área))
+tinta = área / 2
+print('Para pintar essa parede, você precisará de {}L de tinta.'.format(tinta))
+ 
